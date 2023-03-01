b2ed56cd384649aac6f7927145e050430b12a386
rekto167 <ekysr99@gmail.com>
2023-03-01 18:44:48 +0700

    instal tailwindcss, buat login halaman

--- siakad_web_fe/src/App.js
+++ siakad_web_fe/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/auth/Login'
+import Navbar from './components/layouts/Navbar'
+import {Fragment} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return(
+        <Fragment>
+            {/*<Navbar />*/}
+            <Login />
+        </Fragment>
+    )
 }
 
 export default App;
---
+++ siakad_web_fe/src/components/auth/Login/index.js
@@ -0,0 +1,58 @@
+import backgroundnya from "../../../assets/img/bg-img.png"
+import tutwuri from "../../../assets/img/tutwuri.png"
+import {useState} from "react";
+
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen" style={{backgroundImage: `url(${backgroundnya})`, backgroundSize:'cover'}} >
+            <div className="p-8 bg-cyan-400 flex flex-col justify-center items-center rounded-xl">
+                <div className="flex flex-col justify-center items-center">
+                    <img src={tutwuri} className="h-20" />
+                    <span className="text-lg text-center font-bold mt-3">SISTEM INFORMASI AKADEMIK</span>
+                </div>
+                <div className="mt-5 flex flex-col w-full">
+                    <div className="flex flex-col border border-blue-800 p-2 rounded-md">
+                        <span className="font-semibold text-center">Login Sebagai</span>
+                        <div className="flex items-center justify-around mt-3">
+                            <div className="flex items-center self-center mb-2">
+                                <input id="default-radio-1" type="radio" value="" name="default-radio"
+                                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <label htmlFor="default-radio-1"
+                                           className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Guru</label>
+                            </div>
+                            <div className="flex items-center mb-2">
+                                <input id="default-radio-1" type="radio" value="" name="default-radio"
+                                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                <label htmlFor="default-radio-1"
+                                       className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Siswa</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mb-3">
+                        <label className="font-semibold">Username</label>
+                        <input type="text" className="p-3 rounded-md focus:outline-none focus:outline-cyan-600 mt-2" />
+                    </div>
+                    <div className="flex flex-col mb-3">
+                        <label className="font-semibold">Password</label>
+                        <input type={showPassword ? 'text' : 'password'} className="p-3 rounded-md focus:outline-none focus:outline-cyan-600 mt-2" />
+                    </div>
+                    <div className="flex items-center">
+                        <input type="checkbox" checked={showPassword} onChange={handleShowPassword} className="mr-3" />
+                        <label>Lihat Password</label>
+                    </div>
+                    <div className="flex justify-center mt-5">
+                        <button className="p-3 bg-blue-600 px-20 text-white rounded-md font-semibold">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
